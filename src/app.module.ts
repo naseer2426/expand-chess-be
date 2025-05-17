a339d2000af26db30139421f00c056a3e92a916e
naseer2426 <naseerfathima2426@gmail.com>
2025-05-17 20:22:43 +0800

    added pinger and fixed db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,25 @@ import { Module } from '@nestjs/common';
 import { SocketModule } from './socket/socket.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { Game } from './game/game.entity';
+import { PingerModule } from './pinger/pinger.module';
 @Module({
   imports: [
-    ConfigModule.forRoot({}), 
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }), 
     SocketModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.SUPABASE_HOST,
-      port: parseInt(process.env.SUPABASE_PORT, 10),
-      username: process.env.SUPABASE_USERNAME,
-      password: process.env.SUPABASE_PASSWORD,
-      database: process.env.SUPABASE_DATABASE,
-      entities: [],
+      host: process.env.POSTGRESS_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRESS_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRESS_DB,
+      entities: [Game],
       synchronize: true,
-    })
+    }),
+    PingerModule
   ],
 })
 export class AppModule {}
